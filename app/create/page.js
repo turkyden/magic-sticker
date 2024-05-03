3d1a9858229c5ee752c2034660838f2fa0934815
Dengju Deng <wj871287@gmail.com>
2024-05-03 18:11:39 +0800

    feat: 新增账号登录模块

---
+++ app/create/page.js
@@ -0,0 +1,77 @@
+'use client'
+
+import { useState } from "react";
+import Image from "next/image";
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export default function Create() {
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: e.target.prompt.value,
+      }),
+    });
+    let prediction = await response.json();
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+    setPrediction(prediction);
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(1000);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      prediction = await response.json();
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+      console.log({prediction})
+      setPrediction(prediction);
+    }
+  };
+
+  return (
+    <div className="container">
+
+      <p>
+        输入提示词生成
+      </p>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
+        <button type="submit">Go!</button>
+      </form>
+
+      {error && <div>{error}</div>}
+
+      {prediction && (
+        <div>
+            {prediction.output && (
+              <div className="imageWrapper">
+              <Image
+                fill
+                src={prediction.output[prediction.output.length - 1]}
+                alt="output"
+                sizes='100vw'
+              />
+              </div>
+            )}
+            <p>status: {prediction.status}</p>
+        </div>
+      )}
+    </div>
+  );
+}
