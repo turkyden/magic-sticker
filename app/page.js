fc0ad4c5ea75abb1560c8db805890d7a569139ea
Dengju Deng <wj871287@gmail.com>
2024-05-08 12:49:01 +0800

    fix: remove the nav

--- app/page.js
+++ app/page.js
@@ -1,15 +1,5 @@
-'use client'
-import { useUser } from "@clerk/nextjs";
-
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default function Home() {
-
-  const { isLoaded, isSignedIn, user } = useUser();
-
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
 
   return (
     <div className="container">
